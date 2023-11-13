ac9322e7c799b5455b2c98c354d8062230de1123
thiagonunes <thiagonunes@perbank.com.br>
2023-11-13 15:31:04 -0300

    first commit

---
+++ pic-pay-simplificado/src/main/java/br/com/desafio/picpaysimplificado/PicPaySimplificadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.picpaysimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPaySimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPaySimplificadoApplication.class, args);
+	}
+
+}
